0cbf041338ebfa90b84852ac43cf2386c8f2e46f
neaimt <80453432+neaimt@users.noreply.github.com>
2024-07-23 16:07:11 +0900

    init project
    폴더, 파일 생성

---
+++ AnitiatingApp/AnitiatingApp/Application/App/AnitiatingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnitiatingAppApp.swift
+//  AnitiatingApp
+//
+//  Created by 박근경 on 2024/07/23.
+//
+
+import SwiftUI
+
+@main
+struct AnitiatingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnitiatingApp/AnitiatingApp/Application/Main/Record/CheckDetailView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct CheckDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckDetailView()
+}
